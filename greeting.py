7553463d0b4e4f2178da305bcc65e3daa3d416c7
katrina20-meet <katrina20@meet.mit.edu>
2018-07-31 05:17:19 -0400

    Stopping here

---
+++ greeting.py
@@ -0,0 +1,12 @@
+name = input("what is your name?")
+name=name.capitalize()
+print("hello there, " + name)
+name= str(name)
+no_letters=len(name)
+no_letters= str(no_letters)
+print("your name is " + no_letters + " letters long")
+print("the first letter of your name is " + name[0].capitalize() + " and the last letter is " + name[-1].capitalize())
+
+
+
+
